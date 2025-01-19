0ae948bb4890926a49f9cad5ec1f68be060f9399
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2025-01-19 20:02:51 +1000

    Rename and refactor

--- code/Dashboard2/common/enginestats.cpp
+++ code/Dashboard2/common/enginestats.cpp
@@ -1,48 +1,24 @@
 #include "enginestats.h"
-#include <iostream>
 #include <math.h>
 
+//retures BMEP in BAR
+double EngineStats::BMEP_Calculator(int revolutionPerPowerStroke, double torqueNm, double displacementCC)
+{
+    return ((2* M_PI * revolutionPerPowerStroke * torqueNm)/displacementCC)*10;
+}
 
-
-
-
-
-EngineStats::EngineStats() {
-
-
-    //retures BMEP in BAR
-    double BMEP_Calculator(int revolutionPerPowerStroke = 2, double torqueNm = 0, double displacementCC = 0)
+double EngineStats::rpmToRadPerSec(double rpm)
     {
-        return ((2* M_PI * revolutionPerPowerStroke * torqueNm)/displacementCC)10;
+    return rpm *0.10472;
     }
 
-    double rpmToRadPerSec(double rpm)
-        {
-            return rpm 0.10472
-        }
+double EngineStats::power_Calculator(double torqueNm, double RPM)
+{
+    return torqueNm* rpmToRadPerSec(RPM);
+}
 
-    double powerCalculator(double torqueNm, double RPM)
-    {
-        return torque* rpmToRadPerSec(RPM);
-    }
-
-    double BSFC_Calculator(double powerKw, double fuelConsumptionGramsPerSecond)
-    {
-        //Fuel consumption in grams per second / power in watts
-        return fuelConsumptionGramsPerSecond/ (powerKw/1000)
-    }
-
-
-
-    int main() {
-        // Write C++ code here
-        double BMEP = (2* M_PI * 2 * 200)/2000;
-        std::cout << "BMEP " << BMEP_Calculator(2, 200, 2000);
-
-        return 0;
-    }
-    //BMEP calculator
-    //(2 x pi x revolutions per power stroke (2) x torque (nm) ) / displacement = BMEP in mega pascals
-
-
+double EngineStats::BSFC_Calculator(double powerKw, double fuelConsumptionGramsPerSecond)
+{
+    //Fuel consumption in grams per second / power in watts
+    return fuelConsumptionGramsPerSecond/ (powerKw/1000);
 }
--- code/Dashboard2/common/enginestats.h
+++ code/Dashboard2/common/enginestats.h
@@ -4,19 +4,20 @@
 class EngineStats
 {
 public:
-    EngineStats();
 
-    //retures BMEP in BAR
+
+    //calculates Brake Mean Effective Pressure in BAR
     double BMEP_Calculator(int revolutionPerPowerStroke = 2, double torqueNm = 0, double displacementCC = 0);
 
-
+    //Converts RPM to radions per second
     double rpmToRadPerSec(double rpm);
 
+    //Returns power in KW
+    double power_Calculator(double torqueNm, double RPM);
 
-    double powerCalculator(double torqueNm, double RPM);
-
-
+    //calculates the brake specific fuel consumption
     double BSFC_Calculator(double powerKw, double fuelConsumptionGramsPerSecond);
+
 
 
 private:
