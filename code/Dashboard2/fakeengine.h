ca0bee93f7055b80445680e90c483ce2987b3938
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2025-01-19 20:53:07 +1000

    Update fakeengine.h

--- code/Dashboard2/fakeengine.h
+++ code/Dashboard2/fakeengine.h
@@ -4,6 +4,8 @@
 class fakeEngine
 {
 public:
+    //this class will represent a fake engine that generates somewhat realistic values to be used to debug the dashboard UI and other systems.
+
     fakeEngine();
 };
 
