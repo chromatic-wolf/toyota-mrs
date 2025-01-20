7ea8e1dbdac690b507b402ad28dfc9ce5cdec095
Caleb <caleb.davidson@cqumail.com>
2025-01-20 11:57:47 +1000

    created get volumetric efficiency function

---
+++ code/Dashboard2/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appDashboard2_FILE C:/Users/Manager-PC/Documents/GitHub/toyota-mrs/code/Dashboard2/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/appDashboard2.exe)
+set(__QT_DEPLOY_TARGET_appDashboard2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appDashboard2_RUNTIME_DLLS C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Core.dll)
